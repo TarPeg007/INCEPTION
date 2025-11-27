f9f9db4c2f8994cc44a900b1bec1237702524681
Saad fellahi <saad@sfellahi.sfellahi.42.fr>
2025-11-27 07:14:20 -0500

    just checking

--- srcs/requirements/mariadb/tools/mariadbscript.sh
+++ srcs/requirements/mariadb/tools/mariadbscript.sh
@@ -11,10 +11,10 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql --skip-test-db > /dev/null
 fi
 
-# 3. Start temp server to configure users (ALWAYS run this part)
-# We move this outside the 'if' block so we can fix permissions on every restart
+# 3. Start temp server to configure users
 echo "Starting temporary MariaDB server..."
-/usr/bin/mysqld --user=mysql --datadir=/var/lib/mysql --skip-networking &
+# FIX: Use 'mariadbd' (in PATH) instead of /usr/bin/mysqld
+mariadbd --user=mysql --datadir=/var/lib/mysql --skip-networking &
 PID="$!"
 
 # Wait for startup
@@ -22,7 +22,7 @@ until mysqladmin ping >/dev/null 2>&1; do
     sleep 1
 done
 
-# 4. Create Users & Database (ALWAYS run this)
+# 4. Create Users & Database
 echo "Running SQL setup..."
 mariadb <<EOF
 FLUSH PRIVILEGES;
@@ -38,4 +38,5 @@ wait "$PID"
 
 # 5. Start Server
 echo "Starting MariaDB..."
-exec mysqld --user=mysql --bind-address=0.0.0.0
+# FIX: Use 'mariadbd' here too
+exec mariadbd --user=mysql --bind-address=0.0.0.0
